56c6e6f26769503134063560cb917ab0386b2abc
terrellstewart <terrellstewart0@gmail.com>
2018-01-17 10:16:07 -0600

    setting up stuff

---
+++ src/main/java/com/codeup/springbootblog/HomeController.java
@@ -0,0 +1,24 @@
+package com.codeup.springbootblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+public class HomeController {
+
+
+
+        @GetMapping("/hello")
+        @ResponseBody
+        public String hello() {
+            return "Hello from Spring!";
+        }
+
+
+
+
+
+}
